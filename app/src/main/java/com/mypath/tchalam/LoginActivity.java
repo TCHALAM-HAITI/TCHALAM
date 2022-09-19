0890d50316bef12119b5de323ac55832c019eb15
BeliMackendy <belizaire.mackendy@gmail.com>
2022-09-19 12:44:46 -0400

    Add Login and SignUp

---
+++ app/src/main/java/com/mypath/tchalam/LoginActivity.java
@@ -0,0 +1,112 @@
+package com.mypath.tchalam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.GetCallback;
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+public class LoginActivity extends AppCompatActivity {
+    public static final String TAG = "LoginActivity";
+
+    private EditText etEmail;
+    private EditText etPassword;
+    private Button btLogin;
+    private TextView tvSignUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        if (ParseUser.getCurrentUser() != null) {
+            goMainActivity();
+        }
+
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        btLogin = findViewById(R.id.btLogin);
+        tvSignUp = findViewById(R.id.tvSignUp);
+
+
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = etEmail.getText().toString().trim();
+                String password = etPassword.getText().toString().trim();
+
+                if (email.isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "Email Required", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (password.isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "Password Required", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                LoginUser(email, password);
+            }
+        });
+
+        tvSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SignUpUser();
+            }
+        });
+    }
+
+    private void SignUpUser() {
+        Intent i = new Intent(LoginActivity.this, SignUpActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+    private void LoginUser(String email, String password) {
+
+        ParseQuery<ParseUser> query = ParseUser.getQuery();
+        query.whereEqualTo("email", email);
+
+        query.getFirstInBackground(new GetCallback<ParseUser>() {
+            @Override
+            public void done(ParseUser object, ParseException e1) {
+                if (e1 != null) {
+                    Toast.makeText(LoginActivity.this, "Erreur " + e1.getMessage(), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                String username = object.getUsername();
+
+                ParseUser.logInInBackground(username, password, new LogInCallback() {
+                    @Override
+                    public void done(ParseUser user, ParseException e2) {
+                        if (user != null) {
+                            goMainActivity();
+                        } else {
+                            Log.e(TAG, "Échec de la connexion: " + e2.getMessage(), e2);
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+    private void goMainActivity() {
+        Intent i = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+
+}
---
+++ app/src/main/java/com/mypath/tchalam/SignUpActivity.java
@@ -0,0 +1,131 @@
+package com.mypath.tchalam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.CountCallback;
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+import com.parse.SignUpCallback;
+
+public class SignUpActivity extends AppCompatActivity {
+    public static final String TAG = "SignUpActivity";
+
+    private EditText etFirstname;
+    private EditText etLastname;
+    private EditText etEmail;
+    private EditText etPassword;
+    private Button btSignUp;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        etFirstname = findViewById(R.id.etFirstname);
+        etLastname = findViewById(R.id.etLastname);
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        btSignUp = findViewById(R.id.btSignUp);
+
+        btSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = etEmail.getText().toString().trim();
+                String password = etPassword.getText().toString().trim();
+                String firstname = etFirstname.getText().toString().trim();
+                String lastname = etLastname.getText().toString().trim();
+
+                if (email.isEmpty()) {
+                    Toast.makeText(SignUpActivity.this, "Email Obligatoire", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (password.isEmpty()) {
+                    Toast.makeText(SignUpActivity.this, "Mot de passe Obligatoire", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (firstname.isEmpty()) {
+                    Toast.makeText(SignUpActivity.this, "Prénom Obligatoire", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (lastname.isEmpty()) {
+                    Toast.makeText(SignUpActivity.this, "Nom Obligatoire", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                SignUp_User(email, password, lastname, firstname);
+
+            }
+        });
+
+
+    }
+
+    private void SignUp_User(String email, String password, String lastname, String firstname) {
+        ParseQuery<ParseUser> query = ParseUser.getQuery();
+        query.whereEqualTo("email", email);
+
+        query.getFirstInBackground(new GetCallback<ParseUser>() {
+            @Override
+            public void done(ParseUser object, ParseException e) {
+                if (e == null) {
+                    Toast.makeText(SignUpActivity.this, "Erreur : " + object.getEmail() + " Existe", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                String username = lastname+" "+firstname;
+                Log.i(TAG, "done: " + username);
+
+                ParseUser user = new ParseUser();
+
+                user.setUsername(username);
+                user.setPassword(password);
+                user.setEmail(email);
+
+                user.signUpInBackground(new SignUpCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e != null) {
+                            Log.e(TAG, "Échec: ", e);
+                            return;
+                        }
+
+                        ParseUser user1 = ParseUser.getCurrentUser();
+                        user1.put("lastname", lastname);
+                        user1.put("firstname", firstname);
+
+                        user1.saveInBackground(new SaveCallback() {
+                            @Override
+                            public void done(ParseException e) {
+                                if (e != null) return;
+                                goMainActivity();
+                            }
+                        });
+
+                    }
+                });
+            }
+        });
+
+    }
+
+    private void goMainActivity() {
+        Intent i = new Intent(SignUpActivity.this, MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+}
