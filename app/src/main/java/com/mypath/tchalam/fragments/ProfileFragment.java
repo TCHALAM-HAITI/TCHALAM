78c6e9abd423015916e983954af7a40e2eb221ee
BeliMackendy <belizaire.mackendy@gmail.com>
2022-10-26 23:19:14 -0400

    update profile

--- app/src/main/java/com/mypath/tchalam/fragments/ProfileFragment.java
+++ app/src/main/java/com/mypath/tchalam/fragments/ProfileFragment.java
@@ -9,14 +9,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.mypath.tchalam.R;
+import com.parse.ParseUser;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProfileFragment extends Fragment {
 
+    TextView tvFirstName;
+    TextView tvLastName;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -25,7 +29,17 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        tvFirstName = view.findViewById(R.id.tvFirstName);
+        tvLastName = view.findViewById(R.id.tvLastName);
+
+        ParseUser user = ParseUser.getCurrentUser();
+
+        tvFirstName.setText(user.getString("firstname"));
+        tvLastName.setText(user.getString("lastname"));
     }
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
